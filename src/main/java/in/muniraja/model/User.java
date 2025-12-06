e7347d894078c3f3148a3c908060ba2b4585745f
Muniraja <munirajaalakunta2000@gmail.com>
2025-12-06 22:33:45 +0530

    WHY-2879 File Added

---
+++ src/main/java/in/muniraja/model/User.java
@@ -0,0 +1,5 @@
+package in.muniraja.model;
+
+public class User {
+
+}
--- src/main/java/in/muniraja/runner/MyAppRunner.java
+++ src/main/java/in/muniraja/runner/MyAppRunner.java
@@ -9,7 +9,7 @@ public class MyAppRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Welcome to Spring Boot!");
-
+	
 	}
 
 }
