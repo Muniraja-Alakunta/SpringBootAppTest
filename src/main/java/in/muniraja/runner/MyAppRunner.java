04cb73f42d41c3aaab3f3a6ed70ed49ae731d95e
Muniraja <munirajaalakunta2000@gmail.com>
2025-12-06 23:19:02 +0530

    EREF 5468 UPADTE RUNNER WITH LOG4J USING SLF4J

--- src/main/java/in/muniraja/runner/MyAppRunner.java
+++ src/main/java/in/muniraja/runner/MyAppRunner.java
@@ -1,14 +1,32 @@
 package in.muniraja.runner;
 
+import in.muniraja.SpringBootAppTestApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyAppRunner implements CommandLineRunner {
 
+    private final SpringBootAppTestApplication springBootAppTestApplication;
+
+	private static Logger log = LoggerFactory.getLogger(MyAppRunner.class);
+
+    MyAppRunner(SpringBootAppTestApplication springBootAppTestApplication) {
+        this.springBootAppTestApplication = springBootAppTestApplication;
+    }
 	@Override
 	public void run(String... args) throws Exception {
-		
+		int a = 10;
+		System.out.println("Spring Boot!");
+		try {
+			log.info("started");
+		}
+		catch(Exception ex ) {
+			ex.printStackTrace();
+		}
+		log.info("end");
 	}
 
 }
