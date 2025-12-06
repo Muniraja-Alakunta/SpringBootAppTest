fb50779c4e2a324edf615fdfdd64d5cddd1a426a
Muniraja <munirajaalakunta2000@gmail.com>
2025-12-06 22:46:22 +0530

    DTRR-3849 line modified

--- src/main/java/in/muniraja/runner/MyAppRunner.java
+++ src/main/java/in/muniraja/runner/MyAppRunner.java
@@ -8,7 +8,7 @@ public class MyAppRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Welcome to Spring Boot!");
+		
 	}
 
 }
